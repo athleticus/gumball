d6f9e2afb9dfc06d44e1db8e193960d971209642
Ben Martin <ben@benmartinstudios.com.au>
2014-12-10 12:35:15 +1000

    added visits to success

--- screen/templates/success.js
+++ screen/templates/success.js
@@ -1,3 +1,3 @@
-module.exports = function(opts) {
-    this.write_text(["", "", "Hello, " + opts.name + ",", "Enjoy your tasty treat."]);
+module.exports = function(opts)
+	{this.write_text(["","Hello, " + opts.name + ",", "Enjoy your tasty treat.","Visits: " + opts.visits]);
 }
