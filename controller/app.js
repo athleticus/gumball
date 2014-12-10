c1050c33f88f946ccd0b5370b6c554f6e2c49304
Matthew Deering <matt@macbook02.ceit.uq.edu.au>
2014-12-10 11:35:14 +1000

    Added mongodb to controller -todo update visit

--- controller/app.js
+++ controller/app.js
@@ -1,5 +1,23 @@
 var mqtt = require('mqtt'),
     _ = require('underscore');
+
+// connect to mongodb    
+var mongoose = require('mongoose');    
+mongoose.connect('mongodb://winter.ceit.uq.edu.au:27017/gumball');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//create model
+var ds = mongoose.Schema({
+	rfid: String,
+	name: String,
+	visits: {type: Number, default: 0}
+});
+
+var person = mongoose.model('person', ds);
+/// etc.
+
 var config = require('./config.js');
 
 var client = mqtt.createClient(config.port, config.host);
@@ -8,37 +26,56 @@ _.each(config.modules, function(topic, index) {
 	client.subscribe(topic);
 });
 
-var ids = {
+// temp create accounts
+//var matt = new person({rfid : '446779900000000', name: 'Matthew', visits : 0});
+//matt.save(function (err, matt) { if(err) return console.error(err)});
+
+/*var ids = {
 	'64217f0200000000': 'Ben',
 	'42d6c3a00000000': 'Lucas',
 	'446779900000000': 'Matt'
-};
+};*/
 
 var act = function(id) {
-	var name = ids[id];
-	var success = !!name;
+	var success = 0;
+	var newVisits = 0;
 	var tpl, opts;
 
-	if(success) {
-		opts = {
-			tpl: 'success',
-			id: id,
-			name: name
-		};
-	} else {
-		opts = {
-			tpl: 'fail',
-			id: id
-		};
-	}
+	// todo: get entry from db
+	person.findOne({rfid: id}, 'name visits', function(err, person) {
+		if(err) {
+			return console.error(err);
+		} else {
+			if(person != null) {
+				console.log('%s %s %d', person.name, id, person.visits);
+				newVisits = person.visits + 1;
+				opts = {
+					tpl: 'success',
+					id: id,
+					name: person.name,
+					visits: newVisits
+				};
+				person.update({rfid: id}, {$inc : {visits: 1}}, 
+					function (err) {
+						if(err) return console.error(err);
+						
+						person.save(function(err) {
+							if(err) return console.error(err);
+						});
+					});
+				
+			} else {
+				opts = {
+					tpl: 'fail',
+					id: id
+				};
+			}
+			client.publish(config.modules.screen, JSON.stringify(opts));
+			//client.publish(config.modules.dispenser, 'yolo');
+			
+		}
+	})
 
-	// todo: broadcast to screen
-	client.publish(config.modules.screen, JSON.stringify(opts));
-
-	// todo: broadcast to dispenser
-	if(success) {
-		client.publish(config.modules.dispenser, 'yolo');
-	}
 };
 
 client.on('message', function(topic, id) {
