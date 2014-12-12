1a2997201da863c6b950bf4e4e600ad807dc24e2
Matthew Deering <matt@macbook02.ceit.uq.edu.au>
2014-12-12 16:44:56 +1000

    added timeout and lockout

--- controller/app.js
+++ controller/app.js
@@ -19,27 +19,65 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 //create model
 var person = require('./models/person')(mongoose);
+var records = require('./models/records')(mongoose);
 
 var act = function(id) {
 	var success = 0;
 	var newVisits = 0;
 	var tpl, opts;
+	
+	var date = Date.now() - 30000;
+	records.count({rfid: id, timestamp: {$gt: date}}, 
+		function(err, count){
+			if(err) {
+				return console.error(err);
+			} else if (count < 2) {
+				recordVisit(id);
+			} else {
+				opts = {
+					tpl: 'fatty',
+				};
+				client.publish(config.mqtt.topics.screen, JSON.stringify(opts));
+			}
+		});
+}
 
-	// get entry from db
-	person.findOne({rfid: id}, 'name visits', function(err, doc) {
+var recordVisit = function(id) {
+	var record = new records({rfid: id});
+	record.save(function a(err) {
+		if (err) {
+			return console.error(err);
+		} else {
+			//Date 1 week ago
+			date = Date.now() - 180000 //6048000000
+			records.count({rfid: id, timestamp: {$gt: date}}, 
+				function(err, count){
+					if(err) {
+						return console.error(err);
+					} else {
+						//console.log(count);
+						var num = count;
+						swipe(id, num);
+					}
+				});
+		}
+	});
+}
+	
+var swipe = function(id, num) {
+	person.findOne({rfid: id}, 'name visits', function ded(err, doc) {
 		if(err) {
 			return console.error(err);
 		} else {
-			//console.log(typeof doc, doc, doc.name, doc.visits);
 			success = doc != null;
 			if(success) {
-				console.log('%s %s %d', doc.name, id, doc.visits);
+				console.log('%s %s %d %d', doc.name, id, doc.visits, num);
 				newVisits = doc.visits + 1;
 				opts = {
 					tpl: 'success',
 					id: id,
 					name: doc.name,
-					visits: newVisits
+					visits: num
 				};
 				person.update({
 						rfid: id
@@ -52,7 +90,8 @@ var act = function(id) {
 						if(err) {
 							return console.error(err);
 						}
-					});			
+					});
+	
 			} else {
 				opts = {
 					tpl: 'fail',
@@ -69,8 +108,7 @@ var act = function(id) {
 			}
 		}
 	})
-};
-
+}
 client.on('message', function(topic, id) {
 	// ignore non-reader topics
 	if(topic != config.mqtt.topics.reader) {
