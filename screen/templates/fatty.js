8f4692a6bed49f69814bda126ad0effff46c1d95
Ben Martin <ben@benmartinstudios.com.au>
2014-12-12 16:41:51 +1000

    edited templates

---
+++ screen/templates/fatty.js
@@ -0,0 +1,3 @@
+module.exports = function(opts)
+	{this.write_text(["","Hi Fatty,", "I think you have","had enough for now."]);
+}
--- screen/templates/success.js
+++ screen/templates/success.js
@@ -1,3 +1,3 @@
 module.exports = function(opts)
-	{this.write_text(["","Hello, " + opts.name + ",", "Enjoy your tasty treat.","Visits: " + opts.visits]);
+	{this.write_text(["","Hello " + opts.name + ",", "Enjoy your tasty treat.","Visits: " + opts.visits]);
 }
